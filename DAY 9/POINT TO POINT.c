a4843c1482d52cee144131421d73525b41c9700f
Merllinyazh <148896920+Merllinyazh@users.noreply.github.com>
2025-07-05 22:58:38 +0530

    Add files via upload

---
+++ DAY 9/POINT TO POINT.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    // int a=10;
+    // int *p=&a;
+    // int **p1=&p;
+    // printf("Address of A %p\n",p);
+    // printf("Address of p %p\n ",p1);
+    // printf("Addres of p %d\n",**p1);//**p1= *p = *(&a),* and & cancel which gets a data
+    
+    
+    -
+}
